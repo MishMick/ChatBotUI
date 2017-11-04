72b0d2f252dfd64917d3169089433eecc3de2cc3
Mishal <mishgit95@gmail.com>
2017-11-04 19:16:04 +0530

    Grid structure added

---
+++ src/data/users-data.ts
@@ -0,0 +1,386 @@
+export const TableData:Array<any> = [
+  {
+    'name': 'Victoria Cantrell',
+    'position': 'Integer Corporation',
+    'office': 'Croatia',
+    'ext': `0839`
+  }, {
+    'name': 'Pearl Crosby',
+    'position': 'In PC',
+    'office': 'Cambodia',
+    'ext': `8262`
+  }, {
+    'name': 'Colette Foley',
+    'position': 'Lorem Inc.',
+    'office': 'Korea, North',
+    'ext': '8968'
+  }, {
+    'name': 'Anastasia Shaffer',
+    'position': 'Dolor Nulla Semper LLC',
+    'office': 'Suriname',
+    'ext': '7980'
+  }, {
+    'name': 'Gabriel Castro',
+    'position': 'Sed Limited',
+    'office': 'Bahrain',
+    'ext': '0757'
+  }, {
+    'name': 'Cherokee Ware',
+    'position': 'Tincidunt LLC',
+    'office': 'United Kingdom (Great Britain)',
+    'ext': '3995'
+  }, {
+    'name': 'Barry Moss',
+    'position': 'Sociis Industries',
+    'office': 'Western Sahara',
+    'ext': '6697'
+  }, {
+    'name': 'Maryam Tucker',
+    'position': 'Elit Pede Malesuada Inc.',
+    'office': 'Brazil',
+    'ext': '5203'
+  }, {
+    'name': 'Constance Clayton',
+    'position': 'Auctor Velit Aliquam LLP',
+    'office': 'United Arab Emirates',
+    'ext': '4204'
+  }, {
+    'name': 'Rogan Tucker',
+    'position': 'Arcu Vestibulum Ante Associates',
+    'office': 'Jersey',
+    'ext': '0885'
+  }, {
+    'name': 'Emery Mcdowell',
+    'position': 'Gravida Company',
+    'office': 'New Zealand',
+    'ext': '3951'
+  }, {
+    'name': 'Yael Greer',
+    'position': 'Orci Limited',
+    'office': 'Madagascar',
+    'ext': '1416'
+  }, {
+    'name': 'Jared Burgess',
+    'position': 'Auctor Incorporated',
+    'office': 'Burundi',
+    'ext': '4673'
+  }, {
+    'name': 'Sharon Campbell',
+    'position': 'Elit Curabitur Sed Consulting',
+    'office': 'Comoros',
+    'ext': '6274'
+  }, {
+    'name': 'Yeo Church',
+    'position': 'Donec Vitae Erat PC',
+    'office': 'Saudi Arabia',
+    'ext': '0269'
+  }, {
+    'name': 'Kylie Barlow',
+    'position': 'Fermentum Risus Corporation',
+    'office': 'Papua New Guinea',
+    'ext': '2010'
+  }, {
+    'name': 'Nell Leonard',
+    'position': 'Vestibulum Consulting',
+    'office': 'Saudi Arabia',
+    'ext': '4839'
+  }, {
+    'name': 'Brandon Fleming',
+    'position': 'Donec Egestas Associates',
+    'office': 'Poland',
+    'ext': '0622'
+  }, {
+    'name': 'Inga Pena',
+    'position': 'Et Magnis Dis Limited',
+    'office': 'Belgium',
+    'ext': '8140'
+  }, {
+    'name': 'Arden Russo',
+    'position': 'Est Tempor Bibendum Corp.',
+    'office': 'Dominican Republic',
+    'ext': '6774'
+  }, {
+    'name': 'Liberty Gallegos',
+    'position': 'Nec Diam LLC',
+    'office': 'Ghana',
+    'ext': '9266'
+  }, {
+    'name': 'Dennis York',
+    'position': 'Nullam Suscipit Foundation',
+    'office': 'Namibia',
+    'ext': '3133'
+  }, {
+    'name': 'Petra Chandler',
+    'position': 'Pede Nonummy Inc.',
+    'office': 'Namibia',
+    'ext': '3367'
+  }, {
+    'name': 'Aurelia Marshall',
+    'position': 'Donec Consulting',
+    'office': 'Nicaragua',
+    'ext': '2690'
+  }, {
+    'name': 'Rose Carter',
+    'position': 'Enim Consequat Purus Industries',
+    'office': 'Morocco',
+    'ext': '0619'
+  }, {
+    'name': 'Denton Atkins',
+    'position': 'Non Vestibulum PC',
+    'office': 'Mali',
+    'ext': '5806'
+  }, {
+    'name': 'Germaine Osborn',
+    'position': 'Tristique Aliquet PC',
+    'office': 'Lesotho',
+    'ext': '4469'
+  }, {
+    'name': 'Nell Butler',
+    'position': 'Sit Amet Dapibus Industries',
+    'office': 'Cuba',
+    'ext': '7860'
+  }, {
+    'name': 'Brent Stein',
+    'position': 'Eu Augue Porttitor LLP',
+    'office': 'Cyprus',
+    'ext': '4697'
+  }, {
+    'name': 'Alexandra Shaw',
+    'position': 'Aenean Gravida Limited',
+    'office': 'Uruguay',
+    'ext': '1140'
+  },
+  {
+    'name': 'Laura Macias',
+    'position': 'Phasellus Inc.',
+    'office': 'Mauritania',
+    'ext': '2033'
+  }, {
+    'name': 'Nichole Salas',
+    'position': 'Duis PC',
+    'office': 'Madagascar',
+    'ext': '4397'
+  }, {
+    'name': 'Hunter Walter',
+    'position': 'Ullamcorper Duis Cursus Foundation',
+    'office': 'Brazil',
+    'ext': '2227'
+  }, {
+    'name': 'Asher Rich',
+    'position': 'Mauris Ipsum LLP',
+    'office': 'Paraguay',
+    'ext': '7288'
+  }, {
+    'name': 'Angela Carlson',
+    'position': 'Donec Tempor Institute',
+    'office': 'Papua New Guinea',
+    'ext': '5416'
+  }, {
+    'name': 'James Dorsey',
+    'position': 'Ipsum Leo Associates',
+    'office': 'Congo (Brazzaville)',
+    'ext': '6019'
+  }, {
+    'name': 'Wesley Cobb',
+    'position': 'Nunc Est Incorporated',
+    'office': 'Australia',
+    'ext': '6466'
+  }, {
+    'name': 'Meghan Stephens',
+    'position': 'Interdum PC',
+    'office': 'Turkey',
+    'ext': '8001'
+  }, {
+    'name': 'Keely Bauer',
+    'position': 'Nec Tempus Institute',
+    'office': 'Somalia',
+    'ext': '8372'
+  }, {
+    'name': 'Noelani Strong',
+    'position': 'Nec LLP',
+    'office': 'Iran',
+    'ext': '0049'
+  }, {
+    'name': 'Jeanette Henderson',
+    'position': 'Eu Elit Nulla Corporation',
+    'office': 'Italy',
+    'ext': '7586'
+  }, {
+    'name': 'Candace Huber',
+    'position': 'Sed Institute',
+    'office': 'Uganda',
+    'ext': '7183'
+  }, {
+    'name': 'Bethany Potter',
+    'position': 'Vivamus Nibh Dolor Incorporated',
+    'office': 'Puerto Rico',
+    'ext': '3354'
+  }, {
+    'name': 'Whoopi Burks',
+    'position': 'Justo Inc.',
+    'office': 'Fiji',
+    'ext': '2185'
+  }, {
+    'name': 'Sheila Long',
+    'position': 'Diam Associates',
+    'office': 'Sao Tome and Principe',
+    'ext': '7760'
+  }, {
+    'name': 'Sonya Church',
+    'position': 'Laoreet Institute',
+    'office': 'Grenada',
+    'ext': '8920'
+  }, {
+    'name': 'Shaine Forbes',
+    'position': 'Eu Arcu LLP',
+    'office': 'Cyprus',
+    'ext': '2369'
+  }, {
+    'name': 'Alexandra Patrick',
+    'position': 'Ligula Donec Inc.',
+    'office': 'Viet Nam',
+    'ext': '8531'
+  }, {
+    'name': 'Patience Vincent',
+    'position': 'Sem Molestie Associates',
+    'office': 'Philippines',
+    'ext': '8888'
+  }, {
+    'name': 'Evelyn Smith',
+    'position': 'Fusce Industries',
+    'office': 'Togo',
+    'ext': '5051'
+  }, {
+    'name': 'Kieran Gonzalez',
+    'position': 'Non Corp.',
+    'office': 'Equatorial Guinea',
+    'ext': '4834'
+  }, {
+    'name': 'Molly Oneil',
+    'position': 'Non Dui Consulting',
+    'office': 'Belize',
+    'ext': '7501'
+  }, {
+    'name': 'Nigel Davenport',
+    'position': 'Ullamcorper Velit In Industries',
+    'office': 'Vanuatu',
+    'ext': '0976'
+  }, {
+    'name': 'Thor Young',
+    'position': 'Malesuada Consulting',
+    'office': 'French Southern Territories',
+    'ext': '0211'
+  }, {
+    'name': 'Finn Delacruz',
+    'position': 'Lorem Industries',
+    'office': 'Cocos (Keeling) Islands',
+    'ext': '2980'
+  }, {
+    'name': 'Lane Henderson',
+    'position': 'Pede Foundation',
+    'office': 'Kazakhstan',
+    'ext': '1446'
+  }, {
+    'name': 'Shea Potter',
+    'position': 'Curabitur Limited',
+    'office': 'Timor-Leste',
+    'ext': '4654'
+  }, {
+    'name': 'Brynn Yang',
+    'position': 'Ut Limited',
+    'office': 'Mayotte',
+    'ext': '4668'
+  }, {
+    'name': 'Kylan Fuentes',
+    'position': 'Sapien Aenean Associates',
+    'office': 'Brazil',
+    'ext': '6623'
+  }, {
+    'name': 'Lionel Mcbride',
+    'position': 'Ipsum PC',
+    'office': 'Portugal',
+    'ext': '3978'
+  }, {
+    'name': 'Paul Lucas',
+    'position': 'Eget LLP',
+    'office': 'Nicaragua',
+    'ext': '8890'
+  }, {
+    'name': 'Lareina Williamson',
+    'position': 'Imperdiet Ullamcorper Ltd',
+    'office': 'Cocos (Keeling) Islands',
+    'ext': '9489'
+  }, {
+    'name': 'Amy Acevedo',
+    'position': 'Id Institute',
+    'office': 'Cook Islands',
+    'ext': '5592'
+  }, {
+    'name': 'Nomlanga Silva',
+    'position': 'Eget LLC',
+    'office': 'Belize',
+    'ext': '3110'
+  }, {
+    'name': 'Amena Stone',
+    'position': 'Enim Incorporated',
+    'office': 'Guinea',
+    'ext': '1211'
+  }, {
+    'name': 'Danielle Coffey',
+    'position': 'Feugiat Placerat Corp.',
+    'office': 'Sao Tome and Principe',
+    'ext': '8176'
+  }, {
+    'name': 'Buffy Russell',
+    'position': 'Lacus Quisque Ltd',
+    'office': 'Ecuador',
+    'ext': '6741'
+  }, {
+    'name': 'Kaitlin Lamb',
+    'position': 'Malesuada Fringilla Est Associates',
+    'office': 'Algeria',
+    'ext': '5054'
+  }, {
+    'name': 'Leilani Yates',
+    'position': 'Mus Proin LLC',
+    'office': 'South Sudan',
+    'ext': '1550'
+  }, {
+    'name': 'Jemima Moon',
+    'position': 'Phasellus Corp.',
+    'office': 'South Georgia and The South Sandwich Islands',
+    'ext': '7582'
+  }, {
+    'name': 'Hiroko Schwartz',
+    'position': 'Neque Institute',
+    'office': 'Saint Vincent and The Grenadines',
+    'ext': '9368'
+  }, {
+    'name': 'Nathaniel Jensen',
+    'position': 'Mi Tempor Limited',
+    'office': 'Dominica',
+    'ext': '8331'
+  }, {
+    'name': 'Silas Sweeney',
+    'position': 'Ultrices Institute',
+    'office': 'Turkmenistan',
+    'ext': '0746'
+  }, {
+    'name': 'Jermaine Barry',
+    'position': 'Dapibus Corporation',
+    'office': 'Uzbekistan',
+    'ext': '1545'
+
+  }, {
+    'name': 'Tatiana Nichols',
+    'position': 'Nec Diam Industries',
+    'office': 'Cook Islands',
+    'ext': '4395'
+
+  }, {
+    'name': 'Rama Waller',
+    'position': 'Sem Pellentesque LLC',
+    'office': 'Andorra',
+    'ext': '2973'
+  }
+];
